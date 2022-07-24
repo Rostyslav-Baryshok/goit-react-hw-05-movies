bd7a36ce2d3bee3e5ff43ae1a41abc1b69f5be6c
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-07-24 10:33:44 +0300

    base json

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { GlobalStyle } from 'style-config/GlobalStyle';
+
+export const App = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <GlobalStyle />
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from 'App';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './style-config/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
